c1a1f076c7bf53ea5ed94be54879d9e1336f392c
Giovanni Gasparini <74728587+giovannigasparini@users.noreply.github.com>
2025-09-01 17:46:47 +0000

    first_draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config()
+
+with open("style.css") as css_style:
+    st.markdown(f"<style>{css_style.read()}</style>", unsafe_allow_html=True)
+
+
+button_1 = st.button("\n       PASSA        \n")
+
+ph = st.empty()
+N = 50
+for secs in range(N, 0, -1):
+    mm, ss = secs // 60, secs % 60
+    ph.metric("PGP assistant", f"{mm:02d}:{ss:02d}")
+    time.sleep(1)
+
+    if button_1:
+        n = 5 * 60
