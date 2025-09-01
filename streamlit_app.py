459b139663062c477946c226e924102a6bad22a6
Giovanni Gasparini <74728587+giovannigasparini@users.noreply.github.com>
2025-09-01 17:57:04 +0000

    second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,39 @@ import time
 
 st.set_page_config()
 
-with open("style.css") as css_style:
-    st.markdown(f"<style>{css_style.read()}</style>", unsafe_allow_html=True)
+#with open("style.css") as css_style:
+#    st.markdown(f"<style>{css_style.read()}</style>", unsafe_allow_html=True)
 
 
-button_1 = st.button("\n       PASSA        \n")
+st.markdown("""
+    <style>
+        button {
+            width: 100vw;
+            height: 50vh;
+            font-size: 48px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+
+        /* Make sure body doesn't add padding */
+        .main, body, html {
+            margin: 0;
+            padding: 0;
+        }
+
+        @media (min-width: 768px) {
+            .full-width-half-height-button {
+                width: 50vw;  /* Optional: Adjust for tablets/desktops */
+            }
+        }
+    </style>
+
+""", unsafe_allow_html=True)
+
+
+button_1 = st.button("PASSA")
 
 ph = st.empty()
 N = 50
